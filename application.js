3263be3fcde8291420fb20c751ec7e0df2788df9
Sunny Karira <sunnykarira@outlook.com>
2017-07-27 23:55:11 +0530

    Add Routes

--- application.js
+++ application.js
@@ -8,17 +8,27 @@ const http = require('http'),
 
 //Database Connection and token setup
 const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-
-//Require manual setup db functions file
-const db = require('./database/db.js');
-db.createDatabase();
-db.createCollection("user");
 
 
 //Configure Port for deploying on heroku or local.
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10450;
 
+//Require User model
+var User = require('./models/user.js');
+var Product = require('./models/product.js');
+
+//Require Configuration
+const config = require('./config.js');
+
+//Secret
+const secret = config.secret;
+
+//Make Database Connection
+mongoose.Promise = global.Promise;
+mongoose.connect(config.database, {useMongoClient: true});
+const db = mongodb.connection;
 
 //Mimetypes object for writeHead
 const mimeTypes = {
@@ -28,7 +38,8 @@ const mimeTypes = {
     "png": "image/png",
     "js": "text/javascript",
     "css": "text/css",
-    "json": "application/json"
+    "json": "application/json",
+    "text": "text/plain"
 };
 
 //Assets Path
@@ -38,31 +49,98 @@ const assets = {
     "css": "./assets/css/"
 }
 
-function checkJsonObject(data){
-    console.log(data);
+
+
+//Function to check if Password is valid or not and matches with confirm password
+function checkPassword(password, confirmpassword){
+
     //Check for password of 8 or more characters having >1 special characters and >1 uppercase letter
-    let re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    if(data.password === data.confirmpassword && data.password !== ''){
-        if(re.test(data.password) && data.username !== ''){
-            if(db.getUserByUsername(data.username, function(err, user){
-                if(err) throw err;
-                if(user){
-                    console.log('User already exists');
-                    return false;
-                }else{
-                    db.insertUser(data, function(err, user){
-                        if(err) throw err;
-                        if(user){
-                            console.log('User Inserted');
-                            return true;
-                        }
-                    });
-                } 
-            }));
-        }
+    let re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if(password === confirmpassword && password !== '' && password !== undefined){
+            if(re.test(password)){
+                return true;
+            }else{
+                return false;
+            }           
     }
-    
+
 }
+
+function checkPassword(password){
+
+    //Check for password of 8 or more characters having >1 special characters and >1 uppercase letter
+    let re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if(password != '' && password != undefined){
+            if(re.test(password)){
+                return true;
+            }else{
+                return false;
+            }           
+    }
+}
+
+function authMiddleware(username, password, confirmpassword){
+    
+    if(username == undefined || password == undefined || confirmpassword == undefined){
+        return false;
+    }
+
+    if(checkPassword(password, confirmpassword)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function authMiddleware(username, password){
+    if(username == undefined || password == undefined){
+        return false;
+    }
+
+    if(checkPassword(password)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+function generateToken(req, user){
+
+    // By default, expire the token after 7 days.
+    // NOTE: the value for 'exp' needs to be in seconds since
+    // the epoch as per the spec!
+    let expiresDefault = Math.floor(new Date().getTime()/1000) + 7*24*60*60;
+
+    let token = jwt.sign({
+    auth:  user.username,
+    agent: req.headers['user-agent'],
+    exp: expiresDefault
+    }, secret);
+
+    return token;
+}
+
+function tokenMiddleware(token){
+
+    let decoded;
+    try{
+
+        decoded = jwt.verify(token, config.secret);
+    }catch(exception){
+        return false;
+    }
+
+    if(decoded && decoded.auth == localUser.username){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+var localUser = undefined;
+
 
 
 try{
@@ -70,10 +148,9 @@ try{
     http.createServer(function(req, res){
         
         let parsedUrl = url.parse(req.url, true);
-        let path = parsedUrl.pathname;
-        let getFullData = '';
-        let sendFullData = '';
-        if(req.method == 'GET' && path == '/'){
+        let pathname = parsedUrl.pathname;
+
+        if(req.method == 'GET' && pathname == '/'){
 
             fs.readFile(assets.html + 'index.html', function(err, data){
                 if(err){
@@ -84,24 +161,203 @@ try{
                 res.end(data + PORT+ '</p>');
             });
 
-        }else if(req.method == 'POST' && path == '/api/signup'){
+        }else if(req.method == 'POST' && pathname == '/api/signup'){
+
+            var getFullData = '';
 
             req.on('data', function(chunk){
                 getFullData += chunk;
-            });
-            
-            let strategy = checkJsonObject(getFullData);
+            }).on('end', function(){
+                
+                //Parsing into JSON data
+                getFullData = JSON.parse(getFullData);
 
-            req.on('end', function(){
-                
-                res.writeHead(200, {'Content-type': mimeTypes.html});
-                if(strategy){
-                    res.end('User Created.');
+                //Auth strategy to check if username, password exists and are valid or not
+                let passwordCheck = checkPassword(getFullData.password, getFullData.confirmpassword);
+
+                //If auth is valid
+                if(passwordCheck){
+
+                    //Create new user object by mongoose schema
+                    let newUser = new User();
+                    newUser.username = getFullData.username;
+                    newUser.password = getFullData.password;
+
+                    //Add user functionality
+                    User.addUser(newUser, function(err){
+                        res.writeHead(200, {'Content-type': mimeTypes.html});
+                        if(err){
+                            res.end(err);
+                        }else{
+                            res.end('User Added. Please visit /api/authenticate to get the web token.');
+                        }
+                    });
+
                 }else{
-                    res.end('Not able to create user. Please check username or password');
+                    // Invalid Password
+                    res.writeHead(200, {'Content-type': mimeTypes.html});
+                    res.end('Invalid Password.');
                 }
+
+
+            });
+
+        }else if(req.method == 'POST' && pathname == '/api/authenticate'){
+
+            var getFullData = '';
+
+            req.on('data', function(chunk){
+                getFullData += chunk;
+            }).on('end', function(){
+
+                getFullData = JSON.parse(getFullData);
+
+                let passwordCheck = checkPassword(getFullData.password);
+                
+                if(passwordCheck){
+
+                    User.getUser(getFullData, function(err, user){
+
+                        if(user === undefined){
+                            res.writeHead(200, {'Content-type': mimeTypes.html});
+                            res.end('User or Password Incorrect.');
+                        }else{
+
+                            let token = generateToken(req, user);
+                            User.updateToken(user, token, function(err, response){
+                                console.log(response);
+                                if(err) throw err;
+
+                                res.writeHead(200, {'Content-type': mimeTypes.html});
+                                res.end("Your API token is "  + token);
+
+                            });
+                            
+                        }
+
+                    });
+                }else{
+                    res.writeHead(200, {'Content-type': mimeTypes.text});
+                    res.end("Invalid Password.");                    
+                }
+
+            });
+
+
+        }else if(req.method == 'POST' && pathname == '/api/login'){
+            var getFullData = '';
+
+            req.on('data', function(chunk){
+                getFullData += chunk;
+            }).on('end', function(){
+
+                getFullData = JSON.parse(getFullData);
+                let auth = authMiddleware(getFullData.username, getFullData.password);
+
+                if(auth){
+                    User.getUser(getFullData, function(err, user){
+
+                        if(user === undefined){
+                            res.writeHead(200, {'Content-type': mimeTypes.html});
+                            res.end('User not Found');
+                        }else{
+
+                            console.log('You are logged in. Please provide apitoken for next routes.');
+                            localUser = user;
+                            res.writeHead(200, {'Content-type': mimeTypes.text});
+                            res.end('You are logged in. Please provide apitoken for next routes.');
+                            
+                        }
+
+
+                    });
+                }else{
+                    res.writeHead(200, {'Content-type': mimeTypes.text});
+                    res.end('Invalid data entered. Field Missing.');
+                }
+
+            });
+
+        }else if(req.method == 'GET' && pathname == '/api/product'){
+            
+            if(localUser != undefined){
+                let token = req.headers.token || parsedUrl.query.token;
+                let midRes = tokenMiddleware(token);
+                if(midRes){
+                    console.log(parsedUrl.query);
+
+
+                }else{
+                    res.writeHead(200, {'Content-type': mimeTypes.text});
+                    res.end('Please provide correct token in header.');
+                }                
+            }else{
+                    res.writeHead(200, {'Content-type': mimeTypes.text});
+                    res.end('Login to access this route.');                
+            }
+
+
+        }else if(req.method == 'POST' && pathname == '/api/product'){
+
+            var getFullData = '';
+            req.on('data', function(chunk){
+                getFullData += chunk;
+            }).on('end', function(){
+
+                if(localUser != undefined){
+
+                    getFullData = JSON.parse(getFullData);
+                    let token = req.headers.token || parsedUrl.query.token;
+                    let midRes = tokenMiddleware(token);
+                    if(midRes){
+
+                        //let auth = productAddMiddleware(getFullData);
+                        let product = new Product();
+                        Object.assign(product, getFullData);
+        
+                        Product.addProduct(product, function(err){
+                            if(err){
+                                res.writeHead(200, {'Content-type': mimeTypes.text});
+                                //console.log(err);
+                                res.end(err);                              
+                            }else{
+                                res.writeHead(200, {'Content-type': mimeTypes.text});
+                                res.end('Product Added to database');
+                            }
+                                
+                        });
+
+                    }else{
+                        res.writeHead(200, {'Content-type': mimeTypes.text});
+                        res.end('Please provide correct token in header.');                    
+                    }                   
+
+                }else{
+                    res.writeHead(200, {'Content-type': mimeTypes.text});
+                    res.end('Please login to access this route.');                      
+                }
+
                 
             });
+        }else if(req.method == 'DELETE' && pathname == '/api/product'){
+
+            if(localUser != undefined){
+                let token = req.headers.token || parsedUrl.query.token;
+                let midRes = tokenMiddleware(token);
+                if(midRes){
+                    console.log(parsedUrl.query.id);
+
+
+                }else{
+                    res.writeHead(200, {'Content-type': mimeTypes.text});
+                    res.end('Please provide correct token in header.');
+                }                
+            }else{
+                    res.writeHead(200, {'Content-type': mimeTypes.text});
+                    res.end('Login to access this route.');                
+            }            
+
+
         }
 
 
@@ -110,6 +366,6 @@ try{
         console.log("Server started at port " + PORT );
     });
 }catch(exception){
-    res.writeHead(500, {'Content-type':'text/plain' });
+    res.writeHead(500, {'Content-type': mimeTypes.text});
     res.end("Internal Server Error");
 }
